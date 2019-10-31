386b17a729d30da31ae4296c39e441782b854d40
Kristine Rodrigo <krodrigo13@gmail.com>
2019-10-31 07:34:41 -0400

    Temporary Login and Registration Functionality

---
+++ app/src/main/java/com/example/dailyjournal/RegistrationActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dailyjournal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistrationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registration)
+    }
+}
