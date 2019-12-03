fca89d1aace23689de4c99bc6ad3f4386519f6fc
Kristine Rodrigo <krodrigo13@gmail.com>
2019-12-03 09:29:03 -0500

    Updated Calendar

--- app/src/main/java/com/example/dailyjournal/RegistrationActivity.kt
+++ app/src/main/java/com/example/dailyjournal/RegistrationActivity.kt
@@ -4,16 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import android.widget.ProgressBar
+import android.view.View
 
 class RegistrationActivity : AppCompatActivity() {
 
     private var passwordInput: EditText? = null
     private var emailInput: EditText? = null
     private var mAuth: FirebaseAuth? = null
+    private var progressBar: ProgressBar? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,15 +27,20 @@ class RegistrationActivity : AppCompatActivity() {
         mAuth = FirebaseAuth.getInstance()
         emailInput = findViewById<EditText>(R.id.email)
         passwordInput = findViewById<EditText>(R.id.password)
+        progressBar = findViewById(R.id.progressBar)
 
         // Need to add part for person to input their name for the app to call them
 
-        val email = emailInput!!.text.toString()
-        val pass = passwordInput!!.text.toString()
+        // val email = emailInput!!.text.toString()
+        // val pass = passwordInput!!.text.toString()
 
         val button = findViewById<Button>(R.id.register_button)
 
         button.setOnClickListener {
+            progressBar!!.visibility = View.VISIBLE
+            Log.i("testing", "It breaks here")
+            val email = emailInput!!.text.toString()
+            val pass = passwordInput!!.text.toString()
 
             // Also check if email is valid email
             if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)) {
