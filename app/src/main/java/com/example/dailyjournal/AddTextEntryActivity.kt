361063ee39538bb998e36a670878f4e87f21accc
Kristine Rodrigo <krodrigo13@gmail.com>
2019-12-03 11:52:48 -0500

    tmp

--- app/src/main/java/com/example/dailyjournal/AddTextEntryActivity.kt
+++ app/src/main/java/com/example/dailyjournal/AddTextEntryActivity.kt
@@ -1,17 +1,23 @@
 package com.example.dailyjournal
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class AddTextEntryActivity : AppCompatActivity() {
+    private var saveBtn: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_entry)
 
+        saveBtn = findViewById<Button>(R.id.saveButton)
 
-
-
+        saveBtn!!.setOnClickListener {
+            //TODO: integrate with calendar activity and firebase
+            val homeIntent = Intent(this@AddTextEntryActivity, HomeScreenActivity::class.java)
+            startActivity(homeIntent)
+        }
     }
-
-
 }
