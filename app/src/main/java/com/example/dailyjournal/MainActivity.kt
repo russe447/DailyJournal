1afafec34a510c9dd20df9d645f5cd10fdfc9dff
Kristine Rodrigo <krodrigo13@gmail.com>
2019-12-03 18:04:18 -0500

    Fixed Login

--- app/src/main/java/com/example/dailyjournal/MainActivity.kt
+++ app/src/main/java/com/example/dailyjournal/MainActivity.kt
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import android.text.TextUtils
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -19,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private var email: EditText? = null
     private var password: EditText? = null
     private var mAuth: FirebaseAuth? = null
+    private var progressBar: ProgressBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,25 +33,34 @@ class MainActivity : AppCompatActivity() {
         mDatabaseReference = mDatabase!!.reference!!.child("User Information")
         mAuth = FirebaseAuth.getInstance()
 
-        email = findViewById(R.id.email)
-        password = findViewById(R.id.password)
+        progressBar = findViewById(R.id.progressBar)
 
         registerBtn!!.setOnClickListener {
             val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
             startActivity(intent)
         }
 
-        loginBtn!!.setOnClickListener { accountLogin() }
+        //Temporary Access to Home Screen
+        loginBtn!!.setOnClickListener {
+            val intent = Intent(this@MainActivity, HomeScreenActivity::class.java)
+            startActivity(intent) }
+        //loginBtn!!.setOnClickListener { accountLogin() }
     }
 
     private fun accountLogin() {
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+
+        progressBar!!.visibility = View.VISIBLE
         if(TextUtils.isEmpty(email?.text.toString())) {
             Toast.makeText(applicationContext, "Enter email address", Toast.LENGTH_LONG).show()
+            progressBar!!.visibility = View.GONE
             return
         }
 
         if(TextUtils.isEmpty(password?.text.toString())) {
             Toast.makeText(applicationContext, "Enter password", Toast.LENGTH_LONG).show()
+            progressBar!!.visibility = View.GONE
             return
         }
 
@@ -56,12 +68,14 @@ class MainActivity : AppCompatActivity() {
             .addOnCompleteListener {task ->
                 if(task.isSuccessful) {
                     Toast.makeText(applicationContext, "Successful!", Toast.LENGTH_LONG).show()
+                    progressBar!!.visibility = View.GONE
                     val newIntent = Intent(this, SplashScreen::class.java)
                     //newIntent.putExtra(, mAuth?.)
                     startActivity(newIntent)
 
                 } else {
                     Toast.makeText(applicationContext, "Failure", Toast.LENGTH_LONG).show()
+                    progressBar!!.visibility = View.GONE
                 }
             }
     }
