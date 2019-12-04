d0a8b16d32cb635a2762da945e3bea3f92cbbcff
Yeamah Rainsbury <yeamahr@wireless-10-105-3-164.umd.edu>
2019-12-03 23:22:39 -0500

    updated text entry

--- app/src/main/java/com/example/dailyjournal/HomeScreenFragment.kt
+++ app/src/main/java/com/example/dailyjournal/HomeScreenFragment.kt
@@ -9,6 +9,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import java.text.SimpleDateFormat
@@ -20,14 +21,17 @@ class HomeScreenFragment : Fragment() {
 
     private var addImageButton: Button? = null
     private var addEventButton: Button? = null
+    private var saveEntryButton: Button? = null
+    private var writtenEntry: EditText? = null
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         viewOfLayout = inflater.inflate(R.layout.home_screen, container, false)
-        // addTextButton = viewOfLayout.findViewById<Button>(R.id.addText)
+        saveEntryButton = viewOfLayout.findViewById<Button>(R.id.saveText)
         addImageButton = viewOfLayout.findViewById<Button>(R.id.addImage)
         addEventButton = viewOfLayout.findViewById<Button>(R.id.addEvent)
+        writtenEntry = viewOfLayout.findViewById<EditText>(R.id.editText)
 
 
         addImageButton!!.setOnClickListener {
@@ -40,6 +44,12 @@ class HomeScreenFragment : Fragment() {
             startActivityForResult(calIntent, 1)
             //var event = calIntent.getSerializableExtra("event") as Event
 
+        }
+
+        saveEntryButton!!.setOnClickListener {
+            val entryIntent = Intent(activity, AddTextEntryActivity::class.java)
+            entryIntent.putExtra("JOURNAL_ENTRY_TEXT", writtenEntry!!.text.toString())
+            startActivity(entryIntent)
         }
 
         return viewOfLayout
